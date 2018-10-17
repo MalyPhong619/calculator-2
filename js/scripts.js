14697089bf5b71508aca0f2fdd4acf1fc9f55af1
maly phongsavanh and ryan leslie <maly@>
2018-10-17 12:47:30 -0700

    added new js funcction to calculator webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,50 @@
 var add = function(number1, number2){
 return number1 + number2;
 };
-var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("enter another number"));
-alert(add(number1, number2));
 
 var multiply = function(number1, number2){
 return number1 * number2;
 };
-var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("enter another number"));
-alert(multiply(number1, number2));
+
 
 var subtract = function(number1, number2){
 return number1 - number2;
 };
-var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("enter another number"));
-alert(subtract(number1, number2));
+
 
 var divide = function(number1, number2){
 return number1 / number2;
 };
-var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("enter another number"));
-var result = divide(number1, number2);
-alert(result);
+
+
+
+$(document).ready(function() {
+  $(".add").submit(function(event) {
+    event.preventDefault();
+
+    var inputadd1 = parseInt($("#number1Add").val());
+    var inputadd2 = parseInt($("#number2Add").val());
+
+var finalsum = add(inputadd1, inputadd2);
+
+$("#addSum").text(finalsum);
+  });
+$(".subtract").submit(function(event){
+  event.preventDefault();
+  var subtract1 = parseInt($("#subtract1").val());
+  var subtract2 = parseInt($("#subtract2").val());
+  var finalsub = subtract(subtract1, subtract2);
+  $("#subsum").text(finalsub);
+});
+$(".multiply").submit(function(event){
+  event.preventDefault();
+var multiply1 = parseInt($("#multiply1").val());
+var multiply2 = parseInt($("#multiply2").val());
+var finalans = multiply(multiply1, multiply2);
+$("#mult").text(finalans)
+});
+
+
+
+
+});
